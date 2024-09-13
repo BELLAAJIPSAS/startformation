82b82c0865210d9a75cdba8edf4371451db2924e
AHMED BELLAAJ <ahmed.bellaaj@ipsas.tn>
2024-09-13 13:54:44 +0000

    4éme commit

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,20 +1,24 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housing-location';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
     <section class="listing">
-      <img class="listing-photo" {src}="housingLocation,photo" alt="
-      exterior photo of {{housingLocation.name}}">
+
+      <img class="listing-photo" src= "{{housingLocation.photo}}" alt="
+exterior photo of {{housingLocation.name}}">
       <h2 class="listing-heading">{{housingLocation.name}}</h2>
       <p class="listing-location">{{ housingLocation.city}}, {{ housingLocation.state}}</p>
       </section>
+      <a [routerLink]="['/details', housingLocation.id]"> Learn More </a>
   `,
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
   @Input() housingLocation!:HousingLocation;
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import routeConfig from './routes';
+bootstrapApplication(AppComponent,
+  {
+    providers:[
+      provideRouter(routeConfig)
+    ]
+  }
 
-bootstrapApplication(AppComponent)
-  .catch(err => console.error(err));
+)
+.catch(err => console.error(err));
+
